afdbea4b9d9a327942fe3e940624f63993f74638
Skyler Suzuki <skylersuzuk1@gmail.com>
2021-03-02 10:01:45 -0800

    Added Search table view and detail view, added base classes for them. Also created a base custom cell view for events.

---
+++ FRSeatGeek/FRSeatGeek/View Controllers/DetailEventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailEventViewController.swift
+//  FRSeatGeek
+//
+//  Created by Lambda_School_Loaner_204 on 3/2/21.
+//
+
+import UIKit
+
+class DetailEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FRSeatGeek/FRSeatGeek/Views/EventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.swift
+//  FRSeatGeek
+//
+//  Created by Lambda_School_Loaner_204 on 3/2/21.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
